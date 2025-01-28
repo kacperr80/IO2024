f63f5df9d518a24e2f21935bfb66f4a68a1bb19d
Kacper Rumiński <kacperr80@mat.umk.pl>
2025-01-28 22:04:52 +0100

    Dodano sprawdzanie, czy liczba jest dodatnia

--- liczba.cpp
+++ liczba.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+
 int main() {
     int liczba;
     std::cin >> liczba;
+
+    if (liczba <= 0) {
+        std::cout << "Podana liczba nie jest dodatnia. Program zakonczyl dzialanie." << std::endl;
+        return 1;
+    }
+
     std::cout << "Wprowadzona liczba: " << liczba << std::endl;
     return 0;
 }
