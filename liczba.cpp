542bf247f533da9e9940fefd46b105e35091f8dd
Kacper Rumiński <kacperr80@mat.umk.pl>
2025-01-28 21:56:47 +0100

    Dodano funkcję czy_bezkwadratowa i zmodyfikowano program

--- liczba.cpp
+++ liczba.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+bool czy_bezkwadratowa(int liczba){
+	if (liczba < 1) return false;
+	for(int i = 2; i<=std::sqrt(liczba);i++) {
+	if(liczba % (i * i) == 0) return false;
+
+} 
+	return true;
+
+
+
+}
+
+
+
+
 int main() {
     int liczba;
     std::cin >> liczba;
-    std::cout << "Wprowadzona liczba: " << liczba << std::endl;
+    std::cout << "Wprowadzona liczba: " << liczba << (czy_bezkwadratowa(liczba) ? " jest" : " nie jest") << " liczbą bezkwadratową." << std::endl;
     return 0;
 }
